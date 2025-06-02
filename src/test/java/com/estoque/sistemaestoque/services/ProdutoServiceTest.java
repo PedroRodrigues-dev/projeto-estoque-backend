71b24d1b8630d33f06834d3ad758136c531d56dc
Pedro Rodrigues <pedroras004@gmail.com>
2025-06-02 18:56:31 -0300

    Repara testes de produto

--- src/test/java/com/estoque/sistemaestoque/services/ProdutoServiceTest.java
+++ src/test/java/com/estoque/sistemaestoque/services/ProdutoServiceTest.java
@@ -25,9 +25,12 @@ import com.estoque.sistemaestoque.dtos.ProdutoLucroDTO;
 import com.estoque.sistemaestoque.entities.MovimentoEstoque;
 import com.estoque.sistemaestoque.entities.Produto;
 import com.estoque.sistemaestoque.entities.TipoProduto;
+import com.estoque.sistemaestoque.entities.Fornecedor;
 import com.estoque.sistemaestoque.mappers.ProdutoMapper;
 import com.estoque.sistemaestoque.repositories.MovimentoEstoqueRepository;
 import com.estoque.sistemaestoque.repositories.ProdutoRepository;
+import com.estoque.sistemaestoque.repositories.TipoProdutoRepository;
+import com.estoque.sistemaestoque.repositories.FornecedorRepository;
 import com.estoque.sistemaestoque.services.impl.ProdutoServiceImpl;
 
 @ExtendWith(MockitoExtension.class)
@@ -40,6 +43,12 @@ class ProdutoServiceTest {
     private MovimentoEstoqueRepository movimentoEstoqueRepository;
 
     @Mock
+    private TipoProdutoRepository tipoProdutoRepository;
+
+    @Mock
+    private FornecedorRepository fornecedorRepository;
+
+    @Mock
     private ProdutoMapper mapper;
 
     @InjectMocks
@@ -48,18 +57,25 @@ class ProdutoServiceTest {
     private Produto produto;
     private ProdutoDTO produtoDTO;
     private MovimentoEstoque movimento;
+    private TipoProduto tipoProduto;
+    private Fornecedor fornecedor;
 
     @BeforeEach
     void setUp() {
-        TipoProduto tipoProduto = new TipoProduto();
+        tipoProduto = new TipoProduto();
         tipoProduto.setId(1L);
         tipoProduto.setNome("Eletrônico");
+
+        fornecedor = new Fornecedor();
+        fornecedor.setId(1L);
+        fornecedor.setNome("Fornecedor Teste");
 
         produto = new Produto();
         produto.setId(1L);
         produto.setCodigo("PROD001");
         produto.setDescricao("Smartphone");
         produto.setTipoProduto(tipoProduto);
+        produto.setFornecedor(fornecedor);
         produto.setValorFornecedor(new BigDecimal("1000.00"));
         produto.setQuantidadeEstoque(10);
         produto.setAtivo(true);
@@ -69,6 +85,7 @@ class ProdutoServiceTest {
         produtoDTO.setCodigo("PROD001");
         produtoDTO.setDescricao("Smartphone");
         produtoDTO.setTipoProdutoId(1L);
+        produtoDTO.setFornecedorId(1L);
         produtoDTO.setValorFornecedor(new BigDecimal("1000.00"));
         produtoDTO.setQuantidadeEstoque(10);
         produtoDTO.setAtivo(true);
@@ -85,10 +102,26 @@ class ProdutoServiceTest {
     @Test
     void deveSalvarProduto() {
         when(mapper.toEntity(any(ProdutoDTO.class))).thenReturn(produto);
+        when(tipoProdutoRepository.findById(anyLong())).thenReturn(Optional.of(tipoProduto));
+        when(fornecedorRepository.findById(anyLong())).thenReturn(Optional.of(fornecedor));
         when(produtoRepository.save(any(Produto.class))).thenReturn(produto);
         when(mapper.toDTO(any(Produto.class))).thenReturn(produtoDTO);
 
         ProdutoDTO resultado = produtoService.save(produtoDTO);
+
+        assertThat(resultado).isNotNull();
+        assertThat(resultado.getCodigo()).isEqualTo("PROD001");
+    }
+
+    @Test
+    void deveAtualizarProduto() {
+        when(produtoRepository.findById(anyLong())).thenReturn(Optional.of(produto));
+        when(tipoProdutoRepository.findById(anyLong())).thenReturn(Optional.of(tipoProduto));
+        when(fornecedorRepository.findById(anyLong())).thenReturn(Optional.of(fornecedor));
+        when(produtoRepository.save(any(Produto.class))).thenReturn(produto);
+        when(mapper.toDTO(any(Produto.class))).thenReturn(produtoDTO);
+
+        ProdutoDTO resultado = produtoService.update(1L, produtoDTO);
 
         assertThat(resultado).isNotNull();
         assertThat(resultado.getCodigo()).isEqualTo("PROD001");
